597b3aa4a10b307080593b090efb6c49c0d18fde
Artem Schirov <artemschirov@gmail.com>
2021-12-19 05:22:02 +0200

    feat: add eng language to lang.js and add select css

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,21 @@
-const select = document.querySelector('.change-lang');
-const allLang = ['en', 'ru'];
+const select = document.querySelector(".change-lang");
+const allLang = ["en", "ru"];
 
 // go to url with choosed language
 const changeURLLanguage = () => {
   let lang = select.value;
-  location.href = window.location.pathname + '#' + lang; //? узнать разницу location и window.location
+  location.href = window.location.pathname + "#" + lang; //? узнать разницу location и window.location
   location.reload();
-}
+};
 
-const translateHeader = (section, lang) => {
-
-}
+const translateHeader = (section, lang) => {};
 
 const changeLanguage = () => {
   let hash = window.location.hash;
   hash = hash.slice(1);
   // console.log(hash)
   if (!allLang.includes(hash)) {
-    console.log('!allLang')
-    location.href = window.location.pathname + '#en';
+    location.href = window.location.pathname + "#en";
     location.reload();
   }
 
@@ -26,24 +23,20 @@ const changeLanguage = () => {
 
   for (let key in langTextObj) {
     const elem = document.getElementById(key);
-    console.log(elem)
     if (elem) {
       if (elem.childNodes.length > 1) {
         elem.childNodes[0].textContent = langTextObj[key][hash];
       } else {
         elem.textContent = langTextObj[key][hash];
-      };
+      }
     }
-  };
+  }
 
   // for (let key in langImgObj) {
   //   const image = document.getElementById
   // }
-}
-
+};
 
 changeLanguage();
 
-select.addEventListener('change', changeURLLanguage);
-
-
+select.addEventListener("change", changeURLLanguage);
--- scripts/lang.js
+++ scripts/lang.js
@@ -1,309 +1,305 @@
 const langTextObj = {
-  'header-title': {
-    'en': 'Learning how to learn',
-    'ru': 'Научиться учиться',
-  },
-  'header-subtitle': {
-    'en': 'The most effective, cutting-edge learning techniques that you can apply to your daily life',
-    'ru': 'Какие современные и эффективные подходы к обучению вы можете использовать в своей жизни?',
-  },
-  'header-link': {
-    'en': ' Learn more \u{27F6}',
-    'ru': ' Узнать \u{27F6}',
-  },
-  'two-columns-brief-1': {
-    'en': '',
-    'ru': 'Главные проблемы в обучении',
-  },
-  'two-columns-paragraph-1-1': {
-    'en': '',
-    'ru': 'Ни в школе, ни в институте нас не учат тому, как правильно изучать материал. Мы готовимся кэк заменам иучим билеты. Мы тренируемся решать однообразные задачи, чтобы лучше сдать тест, но часто в итоге это недает нам реального знания. Зубрежка быстро выветривается и не приносит пользы.',
-  },
-  'two-columns-span-accent': {
-    'en': '',
-    'ru': 'Вывод:',
-  },
-  'two-columns-paragraph-1-2': {
-    'en': '',
-    'ru': 'учиться тоже нужно уметь, но почему-то этому мало где учат. Что с этим делать?',
-  },
-  'two-columns-paragraph-1-3': {
-    'en': '',
-    'ru': 'Конкретные техники и упражнения помогают изменить подход к обучению, сделать его эффективнымизахватывающим. Эти же техники применяются на примере обучения в Практикуме.',
-  },
-  'section-title-1': {
-    'en': '',
-    'ru': 'Техники обучения',
-  },
-  'section-subtitle-1': {
-    'en': '',
-    'ru': 'Пять практик от Барбары Оакли',
-  },
-  'cards-title-1': {
-    'en': '',
-    'ru': 'Два вида внимания',
-  },
-  'cards-description-1': {
-    'en': '',
-    'ru': 'Глубокие знания возникают, если чередовать сфокусированное и рассеянное мышление.',
-  },
-  'cards-title-2': {
-    'en': 'Recall',
-    'ru': 'Повторение',
-  },
-  'cards-description-2': {
-    'en': '',
-    'ru': 'Вспоминайте изученное — это позволит соединить разрозненные порции памяти.',
-  },
-  'cards-title-3': {
-    'en': '',
-    'ru': 'Интерливинг',
-  },
-  'cards-description-3': {
-    'en': '',
-    'ru': 'Изучайте несколько навыков одновременно, они обогащают друг друга.',
-  },
-  'cards-title-4': {
-    'en': '',
-    'ru': 'Вопросы',
-  },
-  'cards-description-4': {
-    'en': '',
-    'ru': 'Слушая преподавателя, придумывайте хороший вопрос, который вас действительно волнует.',
-  },
-  'cards-title-5': {
-    'en': '',
-    'ru': 'Иллюзия компетентности',
-  },
-  'cards-description-5': {
-    'en': '',
-    'ru': 'Повторите про себя, запишите, расскажите другу: вам только кажется, что вы владеете новой темой.',
-  },
-  'cards-title-6': {
-    'en': '',
-    'ru': '',
-  },
-  'cards-description-6': {
-    'en': '',
-    'ru': '',
-  },
-  'section-title-2': {
-    'en': '',
-    'ru': 'Видео нa TED',
-  },
-  'section-subtitle-2': {
-    'en': '',
-    'ru': 'Для тех, кто любит прокрастинировать',
-  },
-  'two-columns-brief-2': {
-    'en': '',
-    'ru': 'История Барбары Оакли.',
-  },
-  'two-columns-paragraph-2-1': {
-    'en': '',
-    'ru': 'С детства Барбаре не давалась математика. Она считала себя законченным гуманитарием, причём далеко не самым умным. В армии она изучала русский язык, чтобы получить надбавку, да так успешно, что её выдвинули в командиры. Но для продвижения по службе нужно было сдавать математику.И тогда Барбара придумала свой подход к точным наукам. Оказалось, если вам что- то «плохо даётся», ваши добытые трудом знания гораздо глубже, чем у тех, кому всё ясно с первого взгляда.',
-  },
-  'feynman-title': {
-    'en': '',
-    'ru': 'Метод Фейнмана',
-  },
-  'feynman-subtitle': {
-    'en': '',
-    'ru': 'Выучить и не забыть.',
-  },
-  'feynman-link': {
-    'en': '',
-    'ru': 'Подробнее \u{27F6}',
-  },
-  'section-title-3': {
-    'en': '',
-    'ru': 'Цифры и факты',
-  },
-  'section-subtitle-3': {
-    'en': '',
-    'ru': 'Про учёбу и мозг',
-  },
-  'table-heading-1': {
-    'en': '',
-    'ru': '86 миллиардов',
-  },
-  'table-text-1-1': {
-    'en': '',
-    'ru': 'Число нейронов в мозге человека',
-  },
-  'table-heading-2': {
-    'en': '',
-    'ru': '2.1 миллиарда',
-  },
-  'table-text-1-2-1': {
-    'en': '',
-    'ru': 'Число нуждающихся в повышении квалификации',
-  },
-  'table-text-1-2-2': {
-    'en': '',
-    'ru': 'Всемирный Банк, 2017',
-  },
-  'table-heading-3': {
-    'en': '',
-    'ru': '1000 терабайт',
-  },
-  'table-text-1-3': {
-    'en': '',
-    'ru': 'Объём памяти человека',
-  },
-  'table-heading-4': {
-    'en': '',
-    'ru': '500 триллионов',
-  },
-  'table-text-1-4': {
-    'en': '',
-    'ru': 'Число ответственных за обучение нервных синапсов у взрослого человека',
-  },
-  'table-heading-5': {
-    'en': '',
-    'ru': '420 миллионов',
-  },
-  'table-text-1-5-1': {
-    'en': '',
-    'ru': 'Число взрослых людей моложе 25 лет, не имеющих образования для трудоустройства',
-  },
-  'table-text-1-5-2': {
-    'en': '',
-    'ru': 'Всемирный Банк, 2017',
-  },
-  'table-heading-6': {
-    'en': '',
-    'ru': '17–20 лет',
-  },
-  'table-text-1-6': {
-    'en': '',
-    'ru': 'Пик обучаемости',
-  },
-  'table-heading-7': {
-    'en': '',
-    'ru': '1885 год',
-  },
-  'table-text-1-7': {
-    'en': '',
-    'ru': 'Открытие кривой забывания',
-  },
-  'table-heading-8': {
-    'en': '',
-    'ru': '1889 год',
-  },
-  'table-text-1-8': {
-    'en': '',
-    'ru': 'Открытие условного рефлекса',
-  },
-  'khan-author': {
-    'en': '',
-    'ru': 'Салман Хан',
-  },
-  'khan-title': {
-    'en': '',
-    'ru': 'Весь мир - школа',
-  },
-  'khan-quote': {
-    'en': '',
-    'ru': 'Страсть и новаторство Сала Хана меняют процесс обучения миллионов студентов по всему миру. Книгу «Весь мир — школа» нужно прочитать всем, кто занимается образованием — так учащиеся повсюду смогут получить навыки и знания, которые приносят успех в школе, карьере и жизни.',
-  },
-  'khan-quote-author': {
-    'en': '',
-    'ru': 'Джордж Лукас',
-  },
-  'khan-quote-author-subline': {
-    'en': '',
-    'ru': 'Кинорежиссер, продюсер',
-  },
-  'khan-buy-link': {
-    'en': '',
-    'ru': 'Купить книгу \u{27F6}',
-  },
-  'section-title-4': {
-    'en': '',
-    'ru': 'Принципы обучения',
-  },
-  'section-subtitle-4': {
-    'en': '',
-    'ru': 'от Джоша Кауфмана',
-  },
-  'table-text-2-1': {
-    'en': '',
-    'ru': 'Выберите привлекательный проект.',
-  },
-  'table-text-2-2': {
-    'en': '',
-    'ru': 'Сосредоточьтесь на каком-то одном навыке.',
-  },
-  'table-text-2-3': {
-    'en': '',
-    'ru': 'Определите целевой уровень мастерства.',
-  },
-  'table-text-2-4': {
-    'en': '',
-    'ru': 'Разбейте навык на элементы.',
-  },
-  'table-text-2-5': {
-    'en': '',
-    'ru': 'Приготовьте всё необходимое для занятий.',
-  },
-  'table-text-2-6': {
-    'en': '',
-    'ru': 'Устраните препятствия для занятий.',
-  },
-  'table-text-2-7': {
-    'en': '',
-    'ru': 'Выделите специальное время для занятий.',
-  },
-  'table-text-2-8': {
-    'en': '',
-    'ru': 'Создайте быстрые петли обратной связи.',
-  },
-  'table-text-2-9': {
-    'en': '',
-    'ru': 'Занимайтесь по расписанию, короткими интенсивными интервалами.',
-  },
-  'table-text-2-10': {
-    'en': '',
-    'ru': 'Уделяйте внимание количеству и скорости.',
-  },
-  'section-title-5': {
-    'en': '',
-    'ru': 'Полезные ресурсы',
-  },
-  'section-subtitle-5': {
-    'en': '',
-    'ru': 'Больше материалов о техниках и лайфхаках обучения',
-  },
-  'footer-author': {
-    'en': '',
-    'ru': '\u{00A9} 2021 Ɐртём Щиров',
-  },
-  'footer-column-heading-1': {
-    'en': '',
-    'ru': '',
-  },
-  'navigation-list-link-1-1': {
-    'en': '',
-    'ru': '',
-  },
-  'navigation-list-link-1-2': {
-    'en': '',
-    'ru': '',
-  },
-  'navigation-list-link-1-3': {
-    'en': '',
-    'ru': '',
-  },
-  'footer-column-heading-2': {
-    'en': '',
-    'ru': '',
-  },
-  'navigation-list-link-2-1': {
-    'en': '',
-    'ru': '',
-  },
-}
+  "header-title": {
+    en: "Learning how to learn",
+    ru: "Научиться учиться",
+  },
+  "header-subtitle": {
+    en: "The most effective, cutting-edge learning techniques that you can apply to your daily life",
+    ru: "Какие современные и эффективные подходы к обучению вы можете использовать в своей жизни?",
+  },
+  "header-link": {
+    en: " Learn more \u{27F6}",
+    ru: " Узнать \u{27F6}",
+  },
+  "two-columns-brief-1": {
+    en: "The main problem with education",
+    ru: "Главные проблемы в обучении",
+  },
+  "two-columns-paragraph-1-1": {
+    en: "Whether it`s at school or university, the one thing no one ever teaches you is how to teach yourself. We often just end up drilling specific exercises and mindlessly memorizing information so that when exam time comes around, we pass the test. But once the test is over, that information has outlived its usefulness, and there`s no guarantee that information will be retained. In the end, we`re really just learning how to pass our tests.",
+    ru: "Ни в школе, ни в институте нас не учат тому, как правильно изучать материал. Мы готовимся кэк заменам иучим билеты. Мы тренируемся решать однообразные задачи, чтобы лучше сдать тест, но часто в итоге это недает нам реального знания. Зубрежка быстро выветривается и не приносит пользы.",
+  },
+  "two-columns-span-accent": {
+    en: "Here`s the real question: ",
+    ru: "Вывод: ",
+  },
+  "two-columns-paragraph-1-2": {
+    en: "does this approach of repetition and memorization help us truly understand the subject material? The academic consensus nowadays is that it doesn`t. In fact, what`s more important than any individual topic is knowing how to study, but for some reason, this point is often overlooked. So, what can we do to remedy this?",
+    ru: "учиться тоже нужно уметь, но почему-то этому мало где учат. Что с этим делать?",
+  },
+  "two-columns-paragraph-1-3": {
+    en: "Well, there are various techniques and exercises that can be applied to the educational process to help make it more efficient and engaging. Keep scrolling to uncover some of the secrets of effective learning",
+    ru: "Конкретные техники и упражнения помогают изменить подход к обучению, сделать его эффективнымизахватывающим. Эти же техники применяются на примере обучения в Практикуме.",
+  },
+  "section-title-1": {
+    en: "",
+    ru: "Техники обучения",
+  },
+  "section-subtitle-1": {
+    en: "",
+    ru: "Пять практик от Барбары Оакли",
+  },
+  "cards-title-1": {
+    en: "Two Ways Of Thinking",
+    ru: "Два вида внимания",
+  },
+  "cards-description-1": {
+    en: "Deep understanding comes from being able to toggle back and forth between focused and diffused thinking",
+    ru: "Глубокие знания возникают, если чередовать сфокусированное и рассеянное мышление.",
+  },
+  "cards-title-2": {
+    en: "Recall",
+    ru: "Повторение",
+  },
+  "cards-description-2": {
+    en: "Improving your ability to remember key bits of Information helps to connect, different part of the brain,",
+    ru: "Вспоминайте изученное — это позволит соединить разрозненные порции памяти.",
+  },
+  "cards-title-3": {
+    en: "Interleaving",
+    ru: "Интерливинг",
+  },
+  "cards-description-3": {
+    en: "When you learn multiple skills at the same time, they reinforce each other",
+    ru: "Изучайте несколько навыков одновременно, они обогащают друг друга.",
+  },
+  "cards-title-4": {
+    en: "Questions",
+    ru: "Вопросы",
+  },
+  "cards-description-4": {
+    en: "When listening to your teacher, think up a good question, something that you`re really itching to know",
+    ru: "Слушая преподавателя, придумывайте хороший вопрос, который вас действительно волнует.",
+  },
+  "cards-title-5": {
+    en: "Illusion Of Competence",
+    ru: "Иллюзия компетентности",
+  },
+  "cards-description-5": {
+    en: "Test yourself, write down, tell other people, and repeat the process in different settings. Only then can you be trully certain you`ve understood something.",
+    ru: "Повторите про себя, запишите, расскажите другу: вам только кажется, что вы владеете новой темой.",
+  },
+  "cards-title-6": {
+    en: "The Pomodoro Technique",
+    ru: "Методика «Помодоро»",
+  },
+  "cards-description-6": {
+    en: "Time management method. It uses a timer to break work into intervals, 25 minutes, separated by short breaks.",
+    ru: "Увеличение эффективности работы при меньших временных затратах за счёт концентрации и перерывов.",
+  },
+  "section-title-2": {
+    en: "TED talks",
+    ru: "Видео нa TED",
+  },
+  "section-subtitle-2": {
+    en: "For lovers of procrastination",
+    ru: "Для тех, кто любит прокрастинировать",
+  },
+  "two-columns-brief-2": {
+    en: "Barbara Oakley`s story",
+    ru: "История Барбары Оакли.",
+  },
+  "two-columns-paragraph-2-1": {
+    en: "Math didn't come naturally to Barbara in childhood. She thought of herself as more of a humanities person, and not a brilliant one at that. With an eye to becoming a linguist, after school she enlisted in the army, who sent her to learn Russian. After several years of service, during which she rose to the rank of Captain, Barbara left the army and decided to study engineering. In was during this time that she developed her learning methodology as she was forced to reprogram her brain for a different kind of learning. It turns out that when something doesn't come naturally to you, having to work hard to gain the knowledge that other people may get at first glance gives you a deeper, more comprehensive understanding of the subject matter.",
+    ru: "С детства Барбаре не давалась математика. Она считала себя законченным гуманитарием, причём далеко не самым умным. В армии она изучала русский язык, чтобы получить надбавку, да так успешно, что её выдвинули в командиры. Но для продвижения по службе нужно было сдавать математику.И тогда Барбара придумала свой подход к точным наукам. Оказалось, если вам что- то «плохо даётся», ваши добытые трудом знания гораздо глубже, чем у тех, кому всё ясно с первого взгляда.",
+  },
+  "feynman-title": {
+    en: "The Feynman Technique",
+    ru: "Метод Фейнмана",
+  },
+  "feynman-subtitle": {
+    en: "Learn without forgetting",
+    ru: "Выучить и не забыть.",
+  },
+  "feynman-link": {
+    en: "More \u{27F6}",
+    ru: "Подробнее \u{27F6}",
+  },
+  "section-title-3": {
+    en: "On learning and the brain",
+    ru: "Цифры и факты",
+  },
+  "section-subtitle-3": {
+    en: "On learning and the brain",
+    ru: "Про учёбу и мозг",
+  },
+  "table-heading-1": {
+    en: "86 billion",
+    ru: "86 миллиардов",
+  },
+  "table-text-1-1": {
+    en: "The number of neurons in the human brain",
+    ru: "Число нейронов в мозге человека",
+  },
+  "table-heading-2": {
+    en: "73%",
+    ru: "2.1 миллиарда",
+  },
+  "table-text-1-2-1": {
+    en: "The percentage of adults in the us who consider themselves lifelong learners",
+    ru: "Число нуждающихся в повышении квалификации",
+  },
+  "table-text-1-2-2": {
+    en: "pew research center",
+    ru: "Всемирный Банк, 2017",
+  },
+  "table-heading-3": {
+    en: "1,000 terabytes",
+    ru: "1000 терабайт",
+  },
+  "table-text-1-3": {
+    en: "The information storage capacity of a human",
+    ru: "Объём памяти человека",
+  },
+  "table-heading-4": {
+    en: "500 trillion",
+    ru: "500 триллионов",
+  },
+  "table-text-1-4": {
+    en: "The number of nerve synapses responsible for learning in the average adult human",
+    ru: "Число ответственных за обучение нервных синапсов у взрослого человека",
+  },
+  "table-heading-5": {
+    en: "420 million",
+    ru: "420 миллионов",
+  },
+  "table-text-1-5-1": {
+    en: "The number of adults under 25 with insufficient education for finding employment",
+    ru: "Число взрослых людей моложе 25 лет, не имеющих образования для трудоустройства",
+  },
+  "table-text-1-5-2": {
+    en: "World Bank, 2017",
+    ru: "Всемирный Банк, 2017",
+  },
+  "table-heading-6": {
+    en: "26 years old",
+    ru: "17–20 лет",
+  },
+  "table-text-1-6": {
+    en: "The age at which Albert Einstein published his groundbreaking paper on special relativity",
+    ru: "Пик обучаемости",
+  },
+  "table-heading-7": {
+    en: "1885",
+    ru: "1885 год",
+  },
+  "table-text-1-7": {
+    en: "Development of the forgetting curve",
+    ru: "Открытие кривой забывания",
+  },
+  "table-heading-8": {
+    en: "1897",
+    ru: "1889 год",
+  },
+  "table-text-1-8": {
+    en: "Publication of Ivan Pavlov`s research on classical conditioning",
+    ru: "Открытие условного рефлекса",
+  },
+  "khan-author": {
+    en: "Salman Khan",
+    ru: "Салман Хан",
+  },
+  "khan-title": {
+    en: "The One World Schoolhouse",
+    ru: "Весь мир - школа",
+  },
+  "khan-quote": {
+    en: "Sal Khan's passion and innovation is transforming learning for millions of students worldwide. The One World Schoolhouse is a must-read for all who are committed to improving education so students everywhere can gain the skills and knowledge to be successful in school, careers and life.",
+    ru: "Страсть и новаторство Сала Хана меняют процесс обучения миллионов студентов по всему миру. Книгу «Весь мир — школа» нужно прочитать всем, кто занимается образованием — так учащиеся повсюду смогут получить навыки и знания, которые приносят успех в школе, карьере и жизни.",
+  },
+  "khan-quote-author": {
+    en: "George Lucas",
+    ru: "Джордж Лукас",
+  },
+  "khan-quote-author-subline": {
+    en: "Filmmaker",
+    ru: "Кинорежиссер, продюсер",
+  },
+  "khan-buy-link": {
+    en: "Buy book \u{27F6}",
+    ru: "Купить книгу \u{27F6}",
+  },
+  "section-title-4": {
+    en: "Ten principles of rapid skill acquisition",
+    ru: "Принципы обучения",
+  },
+  "section-subtitle-4": {
+    en: "by Josh Kaufman",
+    ru: "от Джоша Кауфмана",
+  },
+  "table-text-2-1": {
+    en: "Choose a lovable project",
+    ru: "Выберите привлекательный проект.",
+  },
+  "table-text-2-2": {
+    en: "Focus your energy on one skill at a time",
+    ru: "Сосредоточьтесь на каком-то одном навыке.",
+  },
+  "table-text-2-3": {
+    en: "Define your target performance level",
+    ru: "Определите целевой уровень мастерства.",
+  },
+  "table-text-2-4": {
+    en: "Deconstruct the skill into sub-skills",
+    ru: "Разбейте навык на элементы.",
+  },
+  "table-text-2-5": {
+    en: "Obtain critical tools",
+    ru: "Приготовьте всё необходимое для занятий.",
+  },
+  "table-text-2-6": {
+    en: "Eliminate barriers to practice",
+    ru: "Устраните препятствия для занятий.",
+  },
+  "table-text-2-7": {
+    en: "Make dedicated time for practice",
+    ru: "Выделите специальное время для занятий.",
+  },
+  "table-text-2-8": {
+    en: "Create fast feedback loops",
+    ru: "Создайте быстрые петли обратной связи.",
+  },
+  "table-text-2-9": {
+    en: "Practice by the clock in short bursts",
+    ru: "Занимайтесь по расписанию, короткими интенсивными интервалами.",
+  },
+  "table-text-2-10": {
+    en: "Emphasize quantity and speed",
+    ru: "Уделяйте внимание количеству и скорости.",
+  },
+  "section-title-5": {
+    en: "Useful resources",
+    ru: "Полезные ресурсы",
+  },
+  "section-subtitle-5": {
+    en: "Further materials on learning techniques and hacks.",
+    ru: "Больше материалов о техниках и лайфхаках обучения",
+  },
+  "footer-author": {
+    en: "\u{00A9} 2021 by Ɐrtem Schirov",
+    ru: "\u{00A9} 2021 Ɐртём Щиров",
+  },
+  "footer-column-heading-1": {
+    en: "About Practicum by Yandex",
+    ru: "О Практикуме",
+  },
+  "navigation-list-link-1-1": {
+    en: "Home page",
+    ru: "Главная",
+  },
+  "navigation-list-link-1-2": {
+    en: "Mission",
+    ru: "Концепция",
+  },
+  "navigation-list-link-1-3": {
+    en: "Professions",
+    ru: "Наставники",
+  },
+  "footer-column-heading-2": {
+    en: "Social media",
+    ru: "Соцсети",
+  },
+};
 
 // 'header__title': {
 //   'en': '',
